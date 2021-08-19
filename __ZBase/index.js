c854d44b1b18e1d7291f0d47c0b640977c0d60f2
louiszen <louiszen.yip@mobinology.com>
2021-08-19 15:15:57 +0800

    Init

---
+++ __ZBase/index.js
@@ -0,0 +1,13 @@
+const Modules = require('./Modules');
+const Utils = require('./Utils');
+const Version = require('./Version');
+const BaseClass = require('./BaseClass');
+const Extensible = require('./Extensible');
+
+module.exports = {
+  BaseClass,
+  Version,
+  Modules,
+  Utils,
+  Extensible
+};
