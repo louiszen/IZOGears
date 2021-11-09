7c5399870f7d0ac5db86bf19afc618c92135e441
Louiszen <louiszen@gmail.com>
2021-11-09 13:10:41 +0800

    COGS

---
+++ index.js
@@ -0,0 +1,11 @@
+const _CoreWheels = require("./_CoreWheels");
+const CommonAPI = require("./CommonAPI");
+const GENERIC = require("./GENERIC");
+const MODULES = require("./MODULES");
+
+module.exports = {
+  _CoreWheels,
+  CommonAPI,
+  GENERIC,
+  MODULES
+};
