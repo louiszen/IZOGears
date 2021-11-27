ff5a9f27f88cd11151e3f8fd0cf4ba5d4f305059
Louiszen <louiszen@gmail.com>
2021-11-27 21:34:28 +0800

    Typedef

---
+++ __Typedef/SYSCredential.js
@@ -0,0 +1,70 @@
+/**
+ * @typedef {{
+ *  Version: String,
+ *  Server: {
+ *    Https?: {
+ *      key: String,
+ *      cert: String,
+ *      intermediate: String,
+ *      passphrase: String
+ *    },
+ *  },
+ *  Email: {
+ *    sender: String,
+ *    user: String,
+ *    pass: String
+ *  },
+ *  SMS: {
+ *    user: String,
+ *    pass: String,
+ *    PATH: String
+ *  },
+ *  Authentication: {
+ *    MSAL?: {
+ *      auth: {
+ *        clientId: String,
+ *        authority: String,
+ *        redirectUri: String
+ *      },
+ *      cache?: {
+ *        cacheLocation: String,
+ *        storeAuthStateInCookie: Boolean
+ *      }
+ *    }
+ *  },
+ *  Authorization: {
+ *    JWT?: {
+ *      TokenSecret: String,
+ *      Expire: Number
+ *    }
+ *  }
+ *  BaseDB: {
+ *    CouchDB?: {
+ *      envs: Object.<string, {
+ *        BASE: String,
+ *        USERNAME: String,
+ *        PASSWORD: String,
+ *        URL: String
+ *      }>
+ *    },
+ *    Cloudant?: {
+ *      envs: Object.<string, {
+ *        USERNAME: String,
+ *        APIKEY: String
+ *      }>
+ *    }, 
+ *    MongoDB?: {
+ *      envs: Object.<string, {
+ *        ConnectString: String,
+ *        DATABASE: String
+ *      } | {
+ *        BASE: String,
+ *        USERNAME: String,
+ *        PASSWORD: String,
+ *        URL: String,
+ *        DATABASE: String
+ *      }>
+ *    }
+ *  }
+ * }} syscredential
+ */
