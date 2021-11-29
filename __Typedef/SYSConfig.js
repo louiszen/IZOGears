1f4a39b823ab92ca5523ac9f3da6ace2e0052354
louiszen <louiszen.yip@mobinology.com>
2021-11-29 13:11:15 +0800

    Typedef

---
+++ __Typedef/SYSConfig.js
@@ -0,0 +1,55 @@
+/**
+ * @typedef {("Username-Password" 
+ *  | "MSAL" | "SMSOTP" | "EmailOTP"
+ *  | "GitHub" | "Facebook" | "Instagram"
+ *  | "Twitter" | "Google" | "LinkedIn")} authMethod
+ * 
+ * @typedef {("CouchDB" | "Cloudant" | "MongoDB")} provider
+ * 
+ * @typedef {{
+ *  General: {
+ *    ID: String,
+ *    Name: String
+ *  },
+ *  Server: {
+ *    Port: Number,
+ *    UseHttps: Boolean,
+ *  },
+ *  Authentication: {
+ *    Method: authMethod | [authMethod],
+ *  },
+ *  Authorization: {
+ *    Method: "JWT",
+ *  },
+ *  Debug: {
+ *    Console: Boolean,
+ *    Color: Boolean,
+ *    InstanceID: Boolean
+ *  },
+ *  Init: {
+ *    OnStart: Boolean,
+ *    CleanDB: Boolean
+ *  },
+ *  BaseDB: {
+ *    Provider: provider,
+ *    Backup: {
+ *      Include?: "All" | [String],
+ *      Exclude?: [String]
+ *    },
+ *  },
+ *  Blob: {
+ *    Provider: "Local",
+ *    Local?: {
+ *      Path: {
+ *        Upload: String,
+ *        Download: String
+ *      }
+ *    }
+ *  },
+ *  LogKeep: {
+ *    Request: Number,
+ *    SignIn: Number,
+ *    Gate: Number
+ *  }
+ * }} sysconfig
+ */
