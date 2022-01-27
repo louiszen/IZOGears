6a0c483f08c0ef658164187757fe15cf1037c447
louiszen <louiszen.yip@mobinology.com>
2022-01-27 10:02:32 +0800

    cpfiles

--- __CodeGen/UpdateCPFiles.js
+++ __CodeGen/UpdateCPFiles.js
@@ -1,48 +1,11 @@
 const Fs = require("../_CoreWheels/Utils/Fs");
 const Chalk = require("../_CoreWheels/Utils/Chalk/Chalk");
 
-var fs = require('fs');
-var path = require('path');
-
-function copyFileSync( source, target ) {
-
-    var targetFile = target;
-
-    // If target is a directory, a new file with the same name will be created
-    if ( fs.existsSync( target ) ) {
-        if ( fs.lstatSync( target ).isDirectory() ) {
-            targetFile = path.join( target, path.basename( source ) );
-        }
-    }
-
-    fs.writeFileSync(targetFile, fs.readFileSync(source));
-}
-
-function copyFolderRecursiveSync( source, target ) {
-  var files = [];
-
-  // Check if folder needs to be created or integrated
-  var targetFolder = path.join( target, path.basename( source ) );
-  if ( !fs.existsSync( targetFolder ) ) {
-      fs.mkdirSync( targetFolder );
-  }
-
-  // Copy
-  if ( fs.lstatSync( source ).isDirectory() ) {
-      files = fs.readdirSync( source );
-      files.forEach( function ( file ) {
-          var curSource = path.join( source, file );
-          if ( fs.lstatSync( curSource ).isDirectory() ) {
-              copyFolderRecursiveSync( curSource, targetFolder );
-          } else {
-              copyFileSync( curSource, targetFolder );
-          }
-      } );
-  }
-}
-
 ( async () => {
-  copyFolderRecursiveSync("./.custom-template", "./IZOGears/__CodeGen/cpfiles/");
-  copyFolderRecursiveSync("./.vscode", "./IZOGears/__CodeGen/cpfiles/");
-  copyFileSync("./package.json", "./IZOGears/__CodeGen/cpfiles/");
+  Fs.copyFolderRecursiveSync("./.custom-template", "./IZOGears/__CodeGen/cpfiles/");
+  console.log(Chalk.Log("[U][v] Updated files to .custom-template to cpfiles. "));
+  Fs.copyFolderRecursiveSync("./.vscode", "./IZOGears/__CodeGen/cpfiles/");
+  console.log(Chalk.Log("[U][v] Updated files to .vscode to cpfiles. "));
+  Fs.copyFileSync("./package.json", "./IZOGears/__CodeGen/cpfiles/");
+  console.log(Chalk.Log("[U][v] Updated files to package.json to cpfiles. "));
 })();
