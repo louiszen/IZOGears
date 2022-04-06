8518751fee3213ebf0bef85e38c1dc8d7f63cd3a
Louiszen <louiszen@gmail.com>
2022-04-06 15:38:23 +0800

    MONGO DB ConnectString

--- _CoreWheels/Modules/Database/NoSQL/MongoDB/MongoDB.js
+++ _CoreWheels/Modules/Database/NoSQL/MongoDB/MongoDB.js
@@ -40,8 +40,8 @@ class MongoDB extends NoSQLDB{
 
     this.config = config;
 
-    if(config.ConnectionString){
-      this.connectURL = config.ConnectionString;
+    if(config.ConnectString){
+      this.connectURL = config.ConnectString;
     }else{
       let {BASE, USERNAME, PASSWORD, URL} = config;
       this.connectURL = (BASE || "mongodb+srv://") + USERNAME + ":" + PASSWORD + "@" + URL;  
